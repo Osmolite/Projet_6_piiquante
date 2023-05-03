9b525f246c04e89b0f3cc218b14e8230b81a984b
Osmolite <trabuoh@gmail.com>
2023-05-03 10:33:39 +0200

    first commit

---
+++ backend/middleware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+ 
+module.exports = (req, res, next) => {
+   try {
+       const token = req.headers.authorization.split(' ')[1];
+       const decodedToken = jwt.verify(token, 'c2Gf9If!9!3s$3YgkB7^F5t5');
+       const userId = decodedToken.userId;
+       req.auth = {
+           userId: userId
+       };
+	next();
+   } catch(error) {
+       res.status(401).json({ error });
+   }
+};
