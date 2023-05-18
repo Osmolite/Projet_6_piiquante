385b4ff7ffd2d825bc9ee9983cf8d680877e3a2e
Osmolite <trabuoh@gmail.com>
2023-05-18 10:22:27 +0200

    version complète

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
-console.log(process.env);
 
 const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
